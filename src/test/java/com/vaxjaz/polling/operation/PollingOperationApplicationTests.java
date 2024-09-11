9ab4811ea06fc00fc52fd42c0346b9dddd0306f0
十八里铺小乞丐 <jie.li@nx-engine.com>
2024-09-11 18:01:45 +0800

    init

---
+++ src/test/java/com/vaxjaz/polling/operation/PollingOperationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vaxjaz.polling.operation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PollingOperationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
