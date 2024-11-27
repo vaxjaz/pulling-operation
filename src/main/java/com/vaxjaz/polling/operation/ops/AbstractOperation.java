e49db247ccc047d07c9cf44b187c01a72a492214
十八里铺小乞丐 <jie.li@nx-engine.com>
2024-11-27 15:15:25 +0800

    fix counter bug

--- src/main/java/com/vaxjaz/polling/operation/ops/AbstractOperation.java
+++ src/main/java/com/vaxjaz/polling/operation/ops/AbstractOperation.java
@@ -72,10 +72,8 @@ public abstract class AbstractOperation<T> implements Operation<T> {
         CompletableFuture<Void> result = doWork(backPressure, apply);
         switch (strategy) {
             case FIXED_THEN_IMMEDIATELY:
-                if (Objects.nonNull(apply)) {
-                    // 如果有值，立马pull，否则走scheduleAtFixedRate
-                    pullImmediately(pullTaskSize, backPressure);
-                }
+                // 如果有值，立马pull，否则走scheduleAtFixedRate
+                pullImmediately(pullTaskSize, backPressure);
                 break;
             case FIXED:
                 // do nothing
